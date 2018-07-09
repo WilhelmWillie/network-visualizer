97cd166673803fdc74d17548e4a39326e9258be2
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-07-08 17:33:26 -0700

    Re-structured how graph is represented for d3.js

--- csv_to_json.py
+++ csv_to_json.py
@@ -10,18 +10,26 @@ companyFrequency = {}
 # Dictionary where key is word and value is frequency in position titles (ex: intern, 12)
 keywordFrequency = {}
 
-def addToConnections(name, company, position):
+# List of Nodes - Needed by D3 JS force directed graph
+nodes = []
+
+# List of links between nodes - Needed by D3 JS force directed graph
+links = []
+
+def addToConnections(name, company, position, email):
   if company in connections:
     connections[company].append({
         'name': name,
-        'position': position
+        'position': position,
+        'email': email
       })
 
     companyFrequency[company] = companyFrequency[company] + 1
   else:
     connections[company] = [{
       'name': name,
-      'position': position
+      'position': position,
+      'email': email
     }]
 
     companyFrequency[company] = 1
@@ -43,24 +51,57 @@ if __name__ == '__main__':
       name = row['First Name'] + ' ' + row['Last Name']
       company = row['Company']
       position = row['Position']
+      email = row['Email Address']
 
       if company == '':
         company = 'N/A'
       if position == '':
         position = 'N/A'
 
-      addToConnections(name, company, position)
+      addToConnections(name, company, position, email)
       processPositionTitle(position)
+
+  # Create node for user
+  nodes.append({
+      'id': 'origin',
+      'label': 'You'
+    })
+
+  # Create nodes for each company and connection (and create links between those)
+  for company in connections:
+    nodes.append({
+        'id': 'company ' + company,
+        'label': company
+      })
+
+    links.append({
+        'source': 'origin',
+        'target': 'company ' + company,
+        'value': companyFrequency[company]
+      })
+
+    for connection in connections[company]:
+      nodes.append({
+          'id': 'connection ' + connection['email'],
+          'label': connection['name']
+        })
+
+      links.append({
+          'source': 'company ' + company,
+          'target': 'connection ' + connection['email'],
+          'value': 1
+        })
 
   # Put processed data into an object for JSON export purposes
   network = {
-    'connections': connections,
+    'nodes': nodes,
+    'links': links,
     'companyFrequency': companyFrequency,
     'keywordFrequency': keywordFrequency
   }
 
   # Export network object to JSON
   with open('network.json', 'w') as jsonFile:
-    json.dump(network, jsonFile)
+    json.dump(network, jsonFile, indent=2)
 
 
